db91e49b5cc726154841d1df16d690fcc2f55498
SECT <info@sectwebstudio.com>
2015-09-04 22:25:36 +0900

    First Commit

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# wp-most-popular-custom
+# WP Most Popular Custom
+
+
+### This Plugin based on WP Most Popular <https://github.com/MattGeri/WP-Most-Popular>
+
+
+#### Installation
+ 1. Download the cfs-loop-field-query.zip file to your computer.  
+ 2. Unzip the file.  
+ 3. Upload the cfs-loop-field-query directory to your /wp-content/plugins/ directory.  
+ 4. Activate the plugin through the 'Plugins' menu in WordPress.  
+ You can access the some setting by going to Settings -> CFS Loop Field Query.
+ 5. Setting "Post Type Name", "Loop Field Name", "Date Field Name in Loop Feld".  
+ That's it. The main query of your select post types will be rewritten.
+
+
+#### Example: Sub Query
+	<ul>
+		<?php if (class_exists('WMP_system')): ?>
+			<?php
+				$rankposts = wmp_get_popular( array(
+					'limit'		=> 5,
+					'post_type'	=> array('fair'),
+					'range'		=> get_option('wmp_range')
+				));
+				$i = 1;
+				if ( count( $rankposts ) > 0 ): foreach ( $rankposts as $post ): setup_postdata( $post );
+			?>
+				<li id="rank-<?php echo sprintf("%02d",$i); ?>">
+					<a href="<?php the_permalink(); ?>">
+						<?php the_title(); ?>
+					</a>
+				</li>
+				<?php $i++; ?>
+			<?php endforeach; endif; wp_reset_postdata(); ?>
+		<?php endif; ?>
+	</ul>
---
+++ admin/index.php
@@ -0,0 +1,73 @@
+<div class="wrap">
+	<h1>Setting Ranking<span style="font-size: 10px; padding-left: 12px;">- For WP MOST POPULAR CUSTOM -</span></h1>
+
+	<?php if(isset($_POST['reset'])): ?>
+		<span style="border: 2px solid #5bd535; border-radius: 5px; color: #888; padding: 3px 10px;">リセットしました</span>
+		<?php
+			function reset_count(){
+				global $wpdb;
+				$results = $wpdb->get_results("
+					TRUNCATE TABLE wp_most_popular
+				");
+				return $results;
+			}
+			reset_count();
+		?>
+	<?php elseif(isset($_GET['settings-updated'])): ?>
+		<span style="border: 2px solid #5bd535; border-radius: 5px; color: #888; padding: 3px 10px;">変更を保存しました</span>
+	<?php endif; ?>
+
+	<section>
+		<form method="post" action="">
+			<table class="form-table">
+	            <tbody>
+	                <tr>
+	                    <th scope="row">
+	                        <label for="wmp_range" style="font-size: 14px; margin: 0;">アクセス数をリセット</label>
+							<p style="color: red; font-size: 10px; margin-top: 0;">全アクセスデータを削除します<br />※この操作はやり直しできません。</p>
+	                    </th>
+	                    <td>
+	                    	<input name="reset" class="button button-primary" type="submit" value="Reset All Count" onClick="window.alert('リセットしますか？')" >
+							<input type="hidden" name="action" value="reset" />
+	                    </td>
+	                </tr>
+	            </tbody>
+	        </table>
+		</form>
+	</section>
+	<section>
+		<form method="post" action="options.php">
+	        <?php
+	            settings_fields( 'ranking-settings-group' );
+				do_settings_sections( 'ranking-settings-group' );
+	        ?>
+	        <table class="form-table">
+	            <tbody>
+	                <tr>
+	                    <th scope="row">
+	                        <label for="wmp_range">レンジを選択してください：</label>
+	                    </th>
+	                    <td>
+	                    	<select id="wmp_range" name="wmp_range">
+								<option value="all_time" <?php if(get_option('wmp_range')=="all_time") echo "selected=selected"; ?>>全期間 (all_time)</option>
+								<option value="monthly" <?php if(get_option('wmp_range')=="monthly") echo "selected=selected"; ?>>1ヶ月 (monthly)</option>
+								<option value="biweekly" <?php if(get_option('wmp_range')=="biweekly") echo "selected=selected"; ?>>2週間 (biweekly)</option>
+								<option value="weekly" <?php if(get_option('wmp_range')=="weekly") echo "selected=selected"; ?>>1週間 (weekly)</option>
+								<option value="daily" <?php if(get_option('wmp_range')=="daily") echo "selected=selected"; ?>>1日 (daily)</option>
+							</select>
+	                    </td>
+	                </tr>
+	                <tr>
+	                    <th scope="row">
+	                        <label for="wmp_loginuser">ログインユーザーのアクセスを除外：</label>
+	                    </th>
+	                    <td>
+	                    	 <input type="checkbox" id="wmp_loginuser" name="wmp_loginuser" <?php if(get_option('wmp_loginuser')=="on") echo 'checked="checked"'; ?>>
+	                    </td>
+	                </tr>
+	            </tbody>
+	        </table>
+	        <?php submit_button(); ?>
+	    </form>
+	</section>
+</div>
