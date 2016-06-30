09f3a64990600bdea8ff0fb07d5b7d14d8c24f98
SECT <info@sectwebstudio.com>
2016-07-01 00:07:54 +0900

    Fix php notice code

--- system/widget.php
+++ system/widget.php
@@ -1,9 +1,9 @@
 <?php
 class WMP_Widget extends WP_Widget {
 	public function __construct() {
-		parent::WP_Widget( 'wmp_widget', 'WP Most Popular', array( 'description' => 'Display your most popular blog posts on your sidebar' ) );
+		parent::__construct( 'wmp_widget', 'WP Most Popular', array( 'description' => 'Display your most popular blog posts on your sidebar' ) );
 	}
-	
+
 	public function form( $instance ) {
 		$defaults = $this->default_options( $instance );
 		?>
@@ -42,18 +42,18 @@ class WMP_Widget extends WP_Widget {
 		</p>
 		<?php
 	}
-	
+
 	private function default_options( $instance ) {
 		if ( isset( $instance[ 'title' ] ) )
 			$options['title'] = esc_attr( $instance[ 'title' ] );
 		else
 			$options['title'] = 'Popular posts';
-			
+
 		if ( isset( $instance[ 'number' ] ) )
 			$options['number'] = (int) $instance[ 'number' ];
 		else
 			$options['number'] = 5;
-		
+
 		if ( isset( $instance[ 'post_type' ] ) )
 			$options['post_type'] = esc_attr( $instance[ 'post_type' ] );
 		else
@@ -63,19 +63,19 @@ class WMP_Widget extends WP_Widget {
 			$options['timeline'] = esc_attr( $instance[ 'timeline' ] );
 		else
 			$options['timeline'] = 'all_time';
-		
+
 		return $options;
 	}
-	
+
 	public function update( $new, $old ) {
 		$instance = wp_parse_args( $new, $old );
 		return $instance;
 	}
-	
+
 	public function widget( $args, $instance ) {
 		// Find default args
 		extract( $args );
-		
+
 		// Get our posts
 		$defaults			= $this->default_options( $instance );
 		$options['limit']	= (int) $defaults[ 'number' ];
@@ -86,7 +86,7 @@ class WMP_Widget extends WP_Widget {
 		}
 
 		$posts = wmp_get_popular( $options );
-		
+
 		// Display the widget
 		echo $before_widget;
 		if ( $defaults['title'] ) echo $before_title . $defaults['title'] . $after_title;
@@ -100,7 +100,7 @@ class WMP_Widget extends WP_Widget {
 		endforeach;
 		echo '</ul>';
 		echo $after_widget;
-		
+
 		// Reset post data
 		wp_reset_postdata();
 	}
